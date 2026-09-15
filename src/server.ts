88a3ed9d4ff07829ab38efdac1d3a768034da8a8
X-Star <shinehtetaung30507@example.com>
2026-09-15 14:31:09 +0630

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
+import path from "path";
 import axios from "axios";
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
